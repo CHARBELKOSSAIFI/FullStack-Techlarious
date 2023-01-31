98ce2b67bbd8ecce6af81a7202195ef2b9a9ca18
charbel kossaifi <94061445+CHARBELKOSSAIFI@users.noreply.github.com>
2023-01-31 18:56:16 +0200

    Update room.js

--- javascript/room.js
+++ javascript/room.js
@@ -1,15 +1,15 @@
-// program for a simple calculator
+// room 2
 
-// take the operator input
+
 const operator = prompt('Enter operator ( either +, -, * or / ): ');
 
-// take the operand input
+
 const number1 = parseFloat(prompt('Enter first number: '));
 const number2 = parseFloat(prompt('Enter second number: '));
 
 let result;
 
-// using if...else if... else
+
 if (operator == '+') {
     result = number1 + number2;
 }
@@ -24,4 +24,5 @@ else {
 }
 
 // display the result
-console.log(`${number1} ${operator} ${number2} = ${result}`);
+//console.log(`${number1} ${operator} ${number2} = ${result}`);
+window.alert(`${number1} ${operator} ${number2} = ${result}`);
