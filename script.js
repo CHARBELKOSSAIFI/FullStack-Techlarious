b0b109a71590400a71ad130e983e863afec8de32
Charbel Kossaifi <charbelkossaifi763@gmail.com>
2023-01-31 18:31:36 +0200

    inital commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const text = ' ';
+const replaceWith = '-';
+const result = 'charbel charbel charbel'.replaceAll(text, replaceWith);
+console.log(result);
+
+
+
+
+let part1="charbel";
+let part2 = "kossaifi";
+let fullname = part1.concat(" ",part2);
+console.log(fullname);
